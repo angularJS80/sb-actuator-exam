ef847a6150bc944cb25db492cae661a171d88978
angularJS80 <bigcen@naver.com>
2019-01-10 22:09:53 +0900

    Initial commit

---
+++ src/main/java/cho/me/actuatortry/ActuatorTryApplication.java
@@ -0,0 +1,14 @@
+package cho.me.actuatortry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorTryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorTryApplication.class, args);
+	}
+
+}
+
